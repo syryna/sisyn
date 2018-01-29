eac10a4991b315499bbae140d8b1f4c61613d28a
syryna <michael.novak@gmx.de>
2018-01-29 16:23:42 +0100

    correct initial site

--- app.js
+++ app.js
@@ -162,10 +162,10 @@ app.get('*', function(req, res, next) {
 });
 
 // Routes for the App
-// app.get("/", ensureAuthenticated, function(req, res) {
-//     // Placeholder
-//     httplog.info('User: ' + res.locals.user.username + ' Type: ' + req.method + ' - Prot: ' + req.protocol + ' Path: ' + req.originalUrl);
-// });
+app.get("/", ensureAuthenticated, function(req, res) {
+    // Placeholder
+    httplog.info('User: ' + res.locals.user.username + ' Type: ' + req.method + ' - Prot: ' + req.protocol + ' Path: ' + req.originalUrl);
+});
 
 // Route files
 let users = require('./routes/users');
